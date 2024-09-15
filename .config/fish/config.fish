d07fee9eaaaec5aa5ae02fd8770df057cb9591e5
radutomy <454451+radutomy@users.noreply.github.com>
2024-09-15 23:53:59 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
-	# Commands to run in interactive sessions can go here
+	set -x ZELLIJ_AUTO_EXIT true
+	set -x ZELLIJ_AUTO_ATTACH true
+	eval (zellij setup --generate-auto-start fish | string collect)
 end
+
 
 # ===== ALIAS ===== #
 
@@ -23,16 +26,15 @@ alias lt='lsd --tree --group-dirs=first'
 
 # ===== SET ===== #
 
-
 set hydro_color_pwd green
 set hydro_color_git yellow
-set fish_prompt_pwd_dir_length 100 # maximum lenght of dir path
+set fish_color_valid_path 				# no text underlying when pressing tab
+set fish_pager_color_prefix 			# no text underying when pressing tab
 
-set fish_color_valid_path
-set fish_pager_color_prefix
+set fish_prompt_pwd_dir_length 100 		# maximum lenght of dir path
 set fish_key_bindings fish_vi_key_bindings
 
-set -gx EDITOR "nvim"  # Use "vim", "code", or another editor
+set EDITOR "nvim"
 
 # when CTRL+F press ENTER to open the file in EDITOR
 set fzf_directory_opts --bind "enter:execute($EDITOR {} &> /dev/tty)"
@@ -40,5 +42,11 @@ set fzf_directory_opts --bind "enter:execute($EDITOR {} &> /dev/tty)"
 # CTRL+F search for file; CTRL+L git status
 fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs
 
+# fzf - find hidden files
+set -g fzf_fd_opts --hidden --max-depth 5
+
 #bind \t accept-autosuggestion
 #bind \t 'commandline -f complete-and-search'
+#bind -M insert \t accept-autosuggestion
+#bind -M insert \t 'commandline -f complete-and-search'
+
