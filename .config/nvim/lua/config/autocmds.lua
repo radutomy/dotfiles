a2f3313be60a5a3ae13de863d4ca7eb5fc61b793
Radu Tomuleasa <radu@rtom.dev>
2024-09-26 17:48:21 +0100

    nvim border

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://www.reddit.com/r/neovim/comments/1ehidxy/you_can_remove_padding_around_neovim_instance
+-- removes border from neovim in the terminal
+local modified = false
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if normal.bg then
+			io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+			modified = true
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+	callback = function()
+		if modified then
+			io.write "\027]111\027\\"
+		end
+	end,
+})
