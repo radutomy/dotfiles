0e2ed68bed975d6c99d14a2aec1c6998eefa1bfe
Radu Tomuleasa <radu@rtom.dev>
2024-09-26 21:03:13 +0100

    nvim tabs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,10 @@
 vim.opt.clipboard = ""
 -- disable auto comment
 vim.cmd [[autocmd FileType * set formatoptions-=ro]]
+
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.smarttab = true
+vim.opt.signcolumn = "yes:1" -- make the gutter smaller
