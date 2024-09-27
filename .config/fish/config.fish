12c7b956a86e6f2006b2031934ee03de186e9524
Radu Tomuleasa <radu@rtom.dev>
2024-09-27 17:35:56 +0100

    fish grep directory C-g

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
 ### Set PATH ###
 
 fish_add_path $HOME/.cargo/bin
+bind \cg _fzf_grep_directory
+
+### Login-Shell-Specific Configs ###
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-### Login-Shell-Specific Configs ###
 if status is-login
     # Commands to run in login sessions can go here
-end
-if status is-interactive
 end
