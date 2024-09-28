e2f915ac3e18b8f7ca147ef4d7b2c9447fe97cbb
Radu Tomuleasa <radu@rtom.dev>
2024-09-28 18:15:29 +0100

    nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,9 +5,12 @@
 -- disable auto comment
 vim.cmd [[autocmd FileType * set formatoptions-=ro]]
 
+--vim.g.rust_recommended_style = false
+
 local o = vim.opt
 
 o.clipboard = ""
+o.autoindent = false
 o.expandtab = false
 o.tabstop = 4
 o.shiftwidth = 4
