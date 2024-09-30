40bd9f076270200b3a7a07022663a702035c1857
Radu Tomuleasa <radu@rtom.dev>
2024-09-30 07:45:48 +0100

    nvim wrap text

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,11 +12,12 @@ local o = vim.opt
 o.clipboard = ""
 o.autoindent = false
 o.expandtab = false
-o.tabstop = 3
-o.shiftwidth = 3
-o.softtabstop = 3
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
 o.smarttab = true
 o.signcolumn = "yes:1" -- make the gutter smaller
+o.wrap = true
 
 o.timeoutlen = 300
 o.ttimeoutlen = 50
