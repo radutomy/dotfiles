354f50dffd552cd4a10d8d07514ab9340158b303
Radu Tomuleasa <radu@rtom.dev>
2024-10-09 21:19:05 +0100

    nvim theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -9,13 +9,12 @@ return {
 				theme = {
 					overrides = function(colors)
 						return {
-							Comment = { fg = colors.grey, bg = "NONE", italic = true }, -- Dark green color for comments
-							-- LineNr = { fg = "#FFC800", bg = "NONE" }, -- Color for normal line numbers
-							CursorLineNr = { fg = "#FFC800", bg = "NONE", bold = true }, -- Color for the current line number
-							Keyword = { fg = colors.blue, bg = "NONE" }, -- Use blue for keywords
-							String = { fg = colors.orange, bg = "NONE" }, -- Use yellow/orange for types
-							Type = { fg = "#4EC9B0", bg = "NONE" }, -- Use yellow/orange for types
-							Function = { fg = "#A53FD6", bg = "NONE" }, -- Use purple for methods
+							Comment = { fg = colors.grey, bg = "NONE", italic = true }, -- Grey color for comments
+							-- CursorLineNr = { fg = "#FFC800", bg = "NONE", bold = true }, -- Bright yellow for the current line number
+							Keyword = { fg = colors.blue, bg = "NONE" }, -- Blue for keywords
+							String = { fg = colors.orange, bg = "NONE" }, -- Orange for strings
+							Type = { fg = "#4EC9B0", bg = "NONE" }, -- Teal for types
+							Function = { fg = "#A53FD6", bg = "NONE" }, -- Purple for functions
 						}
 					end,
 					colors = {
