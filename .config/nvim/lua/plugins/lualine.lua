3482648741f6afddfcdeededade78a4dfe6b66b1
Radu Tomuleasa <radu@rtom.dev>
2024-10-14 16:44:17 +0100

    nvim lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,10 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
+			-- remove date and time from the far-right corner
 			opts.sections.lualine_z = {}
+			-- show only the filename
+			opts.sections.lualine_c = { { "filename", path = 0, icon = "󰈙 ", color = { fg = "#7CB342" } } }
 		end,
 	},
 }
