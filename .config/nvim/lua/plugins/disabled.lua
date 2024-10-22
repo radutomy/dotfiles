7edbbbabf784f624708d423089c96e0a6b60b532
Radu Tomuleasa <radu@rtom.dev>
2024-10-22 11:44:29 +0100

    nvim blink.cmp

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
 	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	-- { "hrsh7th/nvim-cmp", enabled = false },
+	{ "hrsh7th/nvim-cmp", enabled = false },
 }
