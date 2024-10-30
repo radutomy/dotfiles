98421f9274a2a8a305917ab8dc5f8125446c4788
Radu Tomuleasa <radu@rtom.dev>
2024-10-30 14:24:56 +0000

    nvim

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -1,6 +1,38 @@
 return {
-	-- plugin: auto-save.nvim
-	-- function: auto save changes
-	-- src: https://github.com/pocco81/auto-save.nvim
-	"Pocco81/auto-save.nvim",
+	-- Plugin: auto-save.nvim
+	-- Source: https://github.com/pocco81/auto-save.nvim
+	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				enabled = true,
+				execution_message = {
+					message = function()
+						return ""
+					end,
+					dim = 0.18,
+					cleaning_interval = 1250,
+				},
+				trigger_events = { "InsertLeave", "TextChanged" },
+				condition = function(buf)
+					local fn = vim.fn
+					local utils = require "auto-save.utils.data"
+
+					if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+						return true
+					end
+					return false
+				end,
+				write_all_buffers = false,
+				debounce_delay = 135,
+				callbacks = {
+					enabling = nil,
+					disabling = nil,
+					before_asserting_save = nil,
+					before_saving = nil,
+					after_saving = nil,
+				},
+			})
+		end,
+	},
 }
