b11d1ea8b0b7078b363e4d2913f5fffec3469b15
Radu Tomuleasa <radu@rtom.dev>
2024-10-31 16:13:53 +0000

    nvim

--- .config/fish/conf.d/user_vars.fish
+++ .config/fish/conf.d/user_vars.fish
@@ -2,6 +2,7 @@ set fish_greeting # supress fish greeting
 
 set hydro_color_pwd green
 set hydro_color_git yellow
+set hydro_cmd_duration_threshold 10000000
 set fish_color_valid_path # no text underlying when pressing tab
 set fish_pager_color_prefix # no text underying when pressing tab
 
@@ -10,15 +11,15 @@ set fish_prompt_pwd_dir_length 100 # maximum lenght of dir path
 set fish_key_bindings fish_default_key_bindings
 
 set -x EDITOR nvim
-set -x GPG_TTY $(tty)
+set -x GPG_TTY $(tty)
 
 # when CTRL+F press ENTER to open the file in EDITOR
 set fzf_directory_opts --bind "enter:execute($EDITOR {} &> /dev/tty)"
 
 # CTRL+F search for file; CTRL+L git status
 if type -q fzf_configure_bindings
-	# fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --variables=\ck
-	fzf_configure_bindings --directory=\cf --git_status=\cs
+    # fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --variables=\ck
+    fzf_configure_bindings --directory=\cf --git_status=\cs
 end
 
 # fzf - find hidden files
