da936645455bc0efcca6a23b1f14bb11760ce2b2
Radu Tomuleasa <radu@rtom.dev>
2024-11-05 17:42:41 +0000

    nvim

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -18,6 +18,15 @@ return {
 			persist_mode = true,
 			close_on_exit = true,
 			start_in_insert = true,
+
+			-- Custom settings to disable mouse in terminal mode
+			on_open = function()
+				vim.cmd "startinsert" -- Ensure Insert mode on open
+				vim.opt_local.mouse = "" -- Disable mouse for terminal buffer
+			end,
+			on_focus = function()
+				vim.cmd "startinsert" -- Re-enter Insert mode on focus
+			end,
 		},
 	},
 }
