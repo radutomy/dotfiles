64d0b9854af2b07096dcf65a0ef65c20df880a27
Radu Tomuleasa <radu@rtom.dev>
2024-11-07 17:58:57 +0000

    nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -39,10 +39,14 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
 })
 
 -- Force kill terminal when :qa
-vim.api.nvim_create_autocmd("TermOpen", {
-	pattern = "term://*toggleterm#*",
-	callback = function(args)
-		vim.cmd "setlocal bufhidden=hide"
-		vim.b[args.buf].terminal_kill_on_exit = "term"
+vim.api.nvim_create_autocmd("QuitPre", {
+	group = vim.api.nvim_create_augroup("TerminalClose", { clear = true }),
+	callback = function()
+		for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+			if vim.bo[buf].buftype == "terminal" and vim.b[buf].terminal_job_id then
+				vim.fn.jobstop(vim.b[buf].terminal_job_id)
+			end
+		end
 	end,
+	nested = true,
 })
