df44628ff5828053a9357826a1739b6bea6e9b99
Radu Tomuleasa <radu@rtom.dev>
2025-01-23 18:18:06 +0000

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@
 fish_add_path $HOME/.cargo/bin
 bind \cg _fzf_grep_directory
 
+# This is needed for GPG operations that require terminal interaction, like password prompts or PIN entry.
+set -x GPG_TTY (tty)
+
 ### Login-Shell-Specific Configs ###
 
 if status is-interactive
