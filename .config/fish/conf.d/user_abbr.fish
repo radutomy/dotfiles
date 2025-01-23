cfdd92d5c9bf92fab452bc1ec690d1b6b3d36f64
Radu Tomuleasa <radu@rtom.dev>
2025-01-23 18:45:59 +0000

    changer names

--- .config/fish/conf.d/user_abbr.fish
+++ .config/fish/conf.d/user_abbr.fish
@@ -36,10 +36,10 @@ function build_reader
     cargo build --target aarch64-unknown-linux-musl --workspace --manifest-path $CR_PATH/Cargo.toml
 end
 
-abbr -a ter 'ssh ter'
-abbr -a ter1 "$WORK_PATH/login-ter.exp ter1"
-abbr -a ter2 "$WORK_PATH/login-ter.exp ter2"
+abbr -a ter 'ssh local-terasic'
+abbr -a ter1 "$WORK_PATH/login-ter.exp cr1-terasic"
+abbr -a ter2 "$WORK_PATH/login-ter.exp cr2-terasic"
 abbr -a tter "build_reader && scp $CR_PATH/target/aarch64-unknown-linux-musl/debug/reader terasic@10.0.0.2:/home/terasic/rt"
-abbr -a tter1 "build_reader && $WORK_PATH/transfer-ter.exp ter1"
-abbr -a tter2 "build_reader && $WORK_PATH/transfer-ter.exp ter2"
+abbr -a tter1 "build_reader && $WORK_PATH/transfer-ter.exp cr1-terasic"
+abbr -a tter2 "build_reader && $WORK_PATH/transfer-ter.exp cr2-terasic"
 abbr -a cb 'cargo build --target aarch64-unknown-linux-musl --workspace --manifest-path ~/ConceptReader/Cargo.toml'
