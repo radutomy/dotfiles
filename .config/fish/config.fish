c9528e90339a12e7b066e9a1ffb5fe8730feabfb
Radu Tomuleasa <radu@rtom.dev>
2025-01-31 17:53:02 +0000

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ end
 
 if status is-interactive
     # Auto-activate virtual environments only in interactive shells
-    functions -q auto_activate_venv; or source ~/.config/fish/functions/auto_activate_venv.fish
+    #functions -q auto_activate_venv; or source ~/.config/fish/functions/auto_activate_venv.fish
 
     # Set GPG_TTY so GPG can prompt for passphrases in the current TTY
     set -x GPG_TTY (tty)
