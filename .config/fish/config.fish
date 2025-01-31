88c3a019957b852393515af0b6913e88b0dbb909
Radu Tomuleasa <radu@rtom.dev>
2025-01-31 16:00:10 +0000

    config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,15 @@
-functions -q auto_activate_venv; or source ~/.config/fish/functions/auto_activate_venv.fish
-
-### Set PATH ###
-
-fish_add_path $HOME/.cargo/bin
-bind \cg _fzf_grep_directory
-
-# This is needed for GPG operations that require terminal interaction, like password prompts or PIN entry.
-#set -x GPG_TTY (tty)
-
-### Login-Shell-Specific Configs ###
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if status is-login
+    # Set PATH (one time for the entire session)
+    fish_add_path $HOME/.cargo/bin
 end
 
-if status is-login
-    # Commands to run in login sessions can go here
+if status is-interactive
+    # Auto-activate virtual environments only in interactive shells
+    functions -q auto_activate_venv; or source ~/.config/fish/functions/auto_activate_venv.fish
+
+    # Set GPG_TTY so GPG can prompt for passphrases in the current TTY
+    set -x GPG_TTY (tty)
+
+    # Custom key bindings for finding files
+    bind \cg _fzf_grep_directory
 end
