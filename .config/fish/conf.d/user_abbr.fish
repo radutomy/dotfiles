0e5476c134095d515421fd5e5deaa34a1c3c3f43
Radu Tomuleasa <radu@rtom.dev>
2025-03-17 11:22:07 +0000

    nvim fish

--- .config/fish/conf.d/user_abbr.fish
+++ .config/fish/conf.d/user_abbr.fish
@@ -35,13 +35,13 @@ set CR_PATH ~/ConceptReader
 set WORK_PATH ~/.config/work
 
 function build_reader
-    cargo build --target aarch64-unknown-linux-musl --workspace --manifest-path $CR_PATH/Cargo.toml
+    cargo build --target aarch64-unknown-linux-musl --manifest-path $CR_PATH/Cargo.toml
 end
 
 abbr -a ter 'ssh local-terasic'
 abbr -a ter1 "$WORK_PATH/login-ter.exp cr1-terasic"
 abbr -a ter2 "$WORK_PATH/login-ter.exp cr2-terasic"
-abbr -a tter "build_reader && scp $CR_PATH/target/aarch64-unknown-linux-musl/debug/reader terasic@10.0.0.2:/home/terasic/rt"
-abbr -a tter1 "build_reader && $WORK_PATH/transfer-ter.exp cr1-terasic"
-abbr -a tter2 "build_reader && $WORK_PATH/transfer-ter.exp cr2-terasic"
-abbr -a cb 'cargo build --target aarch64-unknown-linux-musl --workspace --manifest-path ~/ConceptReader/Cargo.toml'
+abbr -a qer "build_reader && scp $CR_PATH/target/aarch64-unknown-linux-musl/debug/reader terasic@10.0.0.2:/home/terasic/rt"
+abbr -a qer1 "build_reader && $WORK_PATH/transfer-ter.exp cr1-terasic"
+abbr -a qer2 "build_reader && $WORK_PATH/transfer-ter.exp cr2-terasic"
+abbr -a cb 'cargo build --target aarch64-unknown-linux-musl --manifest-path ~/ConceptReader/Cargo.toml'
