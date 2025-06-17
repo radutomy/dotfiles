6c5f2e89eb42d97f3727931509c282dc477fc7e7
Radu Tomuleasa <radu@rtom.dev>
2025-06-17 19:52:41 +0100

    nvim

--- .config/fish/conf.d/user_abbr.fish
+++ .config/fish/conf.d/user_abbr.fish
@@ -5,7 +5,7 @@ abbr -a sb 'ssh sb'
 abbr -a nas 'ssh nas'
 
 # Directory navigation abbreviations
-abbr -a w "cd /mnt/c/Users/a-rtomuleasa/Downloads"
+abbr -a w "cd /mnt/c/Users/radut/Downloads"
 abbr -a e "explorer.exe ."
 abbr -a g 'cd /mnt/c/gdrive'
 
