e80f126f6f38a5c4587a8c62701fa13b2abd2a3a
Radu T <radu@rtom.dev>
2025-09-11 15:27:27 +0100

    nvim plugin swap

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,4 @@
 return {
 	{ "akinsho/bufferline.nvim", enabled = false },
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "echasnovski/mini.completion", enabled = false },
-	-- { "saghen/blink.cmp", enabled = false },
-	-- { "hrsh7th/nvim-cmp", enabled = false },
+	-- { "echasnovski/mini.completion", enabled = false },
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
 			formatters = {
 				file = {
 					filename_first = true,
+					truncate = 80,
 				},
 			},
 		},
@@ -13,6 +14,29 @@ return {
 			win = {
 				position = "float",
 			},
+		},
+	},
+	keys = {
+		{
+			"<leader>fF",
+			function()
+				Snacks.picker.files({ cwd = LazyVim.root() })
+			end,
+			desc = "Find Files (Root Dir)",
+		},
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.getcwd() })
+			end,
+			desc = "Find Files (cwd)",
+		},
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.getcwd() })
+			end,
+			desc = "Find Files (cwd)",
 		},
 	},
 }
