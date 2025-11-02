0732d270221ea1e0b590d1aba2f4eb6ccdc0c3f9
Radu T <radu@rtom.dev>
2025-11-02 05:37:26 +0000

    nvim

--- .config/nvim/lua/plugins/copilot-chat.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	opts = {
-		sticky = "#buffer", -- This adds #buffer as a sticky prompt to every chat
-		-- selection = require("CopilotChat.select").buffer,
-	},
-}
