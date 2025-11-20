8d468d1a715b1e9dcafc7206a95ac6c32b900eac
Radu T <radu@rtom.dev>
2025-11-20 12:00:17 +0000

    lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,14 @@ return {
 	opts = {
 		diagnostics = {
 			virtual_text = false,
-			signs = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "●",
+					[vim.diagnostic.severity.WARN] = "●",
+					[vim.diagnostic.severity.HINT] = "●",
+					[vim.diagnostic.severity.INFO] = "●",
+				},
+			},
 			float = { border = "rounded" },
 		},
 	},
