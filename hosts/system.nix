d7706a5777b8c601eaf4100010807507c6a4f006
Radu T <radu@rtom.dev>
2026-02-12 15:48:52 +0000

    chmod

--- hosts/system.nix
+++ hosts/system.nix
@@ -2,7 +2,6 @@
 {
   virtualisation.docker.enable = true;
   users.users.root.shell = pkgs.zsh;
-  users.users.root.group = "users";
   users.users.root.homeMode = "775";
   programs.zsh.enable = true;
 }
