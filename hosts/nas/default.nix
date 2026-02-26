d5843a2c203bab45a81ae8f27d592e711ef87071
Radu T <radu@rtom.dev>
2026-02-26 00:33:44 +0000

    nas

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -8,6 +8,7 @@
 
   programs.zsh.initContent = ''
     if [[ -z "$TMUX" && $- == *i* ]]; then
-      tmux new-session -A -s main \; set status-position bottom \; set status-right "#[fg=green] 󰒍 NAS   " \; setw window-status-format "" \; setw window-status-current-format "" \; set pane-border-status off    fi
+      tmux new-session -A -s main \; set status-position bottom \; set status-right "#[fg=green] 󰒍 NAS   " \; setw window-status-format "" \; setw window-status-current-format "" \; set pane-border-status off
+    fi
   '';
 }
