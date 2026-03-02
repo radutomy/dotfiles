929033cec7bb1e3ca83d03008d13771cd40283d1
Radu T <radu@rtom.dev>
2026-03-02 01:06:24 +0000

    tmux

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -7,12 +7,11 @@
   ];
 
   home.sessionVariables.HOST_ICON = "󰒍";
-
   programs.zsh.shellAliases.tmux = "command tmux -f ${config.xdg.configHome}/tmux/tmux.nas.conf";
 
-  programs.zsh.initContent = ''
-    if [[ -z "$TMUX" && $- == *i* ]]; then
-      exec tmux new-session -A -s main \; set status off \; set pane-border-status off
-    fi
-  '';
+  # programs.zsh.initContent = ''
+  # if [[ -z "$TMUX" && $- == *i* ]]; then
+  # exec tmux new-session -A -s main \; set status off \; set pane-border-status off
+  # fi
+  # '';
 }
