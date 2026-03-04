d55777685783a54d79f202b05b517135ddd61ab8
Radu T <radu@rtom.dev>
2026-03-04 15:42:15 +0000

    rm chat and proton-rust

--- modules/work.nix
+++ modules/work.nix
@@ -14,29 +14,7 @@ let
       path = "proton/clients/monorepo";
       branch = "main";
     }
-    {
-      path = "chat/chat-client";
-      branch = "develop";
-    }
-    {
-      path = "rust/proton-rust";
-      branch = "master";
-    }
   ];
-
-  # for chat-client - bind to a specific version
-  wasm-bindgen-cli = pkgs.rustPlatform.buildRustPackage {
-    pname = "wasm-bindgen-cli";
-    version = "0.2.106";
-    src = pkgs.fetchCrate {
-      pname = "wasm-bindgen-cli";
-      version = "0.2.106";
-      hash = "sha256-M6WuGl7EruNopHZbqBpucu4RWz44/MSdv6f0zkYw+44=";
-    };
-    cargoHash = "sha256-ElDatyOwdKwHg3bNH/1pcxKI7LXkhsotlDPQjiLHBwA=";
-    nativeBuildInputs = [ pkgs.pkg-config ];
-    buildInputs = [ pkgs.openssl ];
-  };
 
   cloneRepo =
     { path, branch }:
@@ -53,22 +31,16 @@ in
 {
   home = {
     packages = with pkgs; [
-      # proton-rust
+      # mail
       go
       llvmPackages.libclang
 
-      # chat-client
-      pnpm
-      wasm-pack
-      wasm-bindgen-cli
-      lld
       docker
     ];
 
     sessionVariables = {
       GOPATH = "${config.home.homeDirectory}/.local/share/go";
       LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-      BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.glibc.dev}/include";
     };
 
     activation.cloneWorkRepos = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
